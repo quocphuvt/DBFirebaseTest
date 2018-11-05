23a41cf3e295f1f3a0de041319a98e52f50df034
quocphuvt <lequocphuvt0901@gmail.com>
2018-11-05 07:50:38 +0700

    Add new

--- app/src/main/java/com/example/quocphu/dbfirebasetest/MainActivity.java
+++ app/src/main/java/com/example/quocphu/dbfirebasetest/MainActivity.java
@@ -14,9 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText et;
@@ -52,53 +50,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //nut chuoi
-        DatabaseReference nut_chuoi=database.getReference("nutchuoi");
-        nut_chuoi.setValue("nut chuoi moi");
+        DatabaseReference nut_user = database.getReference("users");
+        List<User> ds_user = new ArrayList<>();
+        ds_user.add(new User("quocphu","lequocphu"));
+        ds_user.add(new User("quocphong","lequocphong"));
+        for(int i = 0; i<ds_user.size();i++){
+            nut_user.push().setValue(ds_user.get(i));
+        }
 
-//nut so
-        DatabaseReference nut_so=database.getReference("nutso");
-        nut_so.setValue(8);
 
-//nut Map
-        DatabaseReference nut_map=database.getReference("nutmap");
-        Map<String,Object> m=new HashMap<String,Object>();
-        m.put("tensv","nguyen van teo");
-        m.put("tuoi",5);
-        m.put("diachi","30/31 duong 3 thang 2");
-        nut_map.setValue(m);
 
-//nut List
-        DatabaseReference nut_list=database.getReference("nut_list");
-        List<String> list=new ArrayList<String>();
-        list.add("muc thu 1");
-        list.add("muc thu 2");
-        nut_list.setValue(list);
-        //nut conCho
-        DatabaseReference nut_list_cho=database.getReference("nut_list_cho");
-        List<ConCho> list_cho=new ArrayList<ConCho>();
-        list_cho.add(new ConCho("ki","trang xam",2));
-        list_cho.add(new ConCho("na","vang den",4));
-        nut_list_cho.setValue(list_cho);
+
+
 
 
 
 
     }
-    public class ConCho {
-        public String key;
-        public String ten;
-        public String maulong;
-        public int tuoi;
-        public ConCho()
-        {
-            //phai co 1 ham tao khong doi so
-        }
-        public ConCho(String ten, String maulong,int tuoi)
-        {
-            this.ten=ten;
-            this.tuoi=tuoi;
-            this.maulong=maulong;
-        }
-    }
+
+
 }
---
+++ app/src/main/java/com/example/quocphu/dbfirebasetest/User.java
@@ -0,0 +1,11 @@
+package com.example.quocphu.dbfirebasetest;
+
+public class User {
+    public String userName;
+    public String passWord;
+
+    public User(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+}
